9c67440aaf56dd8cd724b66272c141f4188e37eb
Omar Trevino <omar.dintec@gmail.com>
2020-06-23 12:06:17 +0800

    changed the views and controller paths

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,6 +5,15 @@ class RestaurantsController < ApplicationController
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(params[:restaurant])
+    @restaurant.save
+  end
 end
