21f90cd0934fc14ef56a8dec749dbc2da5182c62
Omar Trevino <omar.dintec@gmail.com>
2020-06-23 10:08:29 +0800

    added the controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # CRUD
+  # CREATE
+  # verb + path , to: "controller_name#action"
+  # Read All restaurants
+  # get '/restaurants', to: 'restaurants#index'
+
+  # # Read one restaurant
+  # get '/restaurants/:id', to: 'restaurants#show'
+
+  # # Create a restaurants
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post '/restaurants', to: 'restaurants#create'
+
+  # # Update a restaurant
+  # get '/restaurants/:id/edit', to: 'restaurants#edit'
+  # patch '/restaurants/:id', to: 'restaurants#update'
+
+  # # Delete a restaurant
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
+
+  # MAGIC Comment
+  resources :restaurants
 end
